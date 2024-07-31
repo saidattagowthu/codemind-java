81e54771861daa71fbfcc9ce7f44f13f9b543635
sai datta manikanta <147487732+saidattagowthu@users.noreply.github.com>
2024-07-31 15:26:24 +0530

    import java.util.*; public class division {     public static void main(String[] args)     {         int a,b;         Scanner read=new Scanner(System.in);         a=read.nextInt();         b=read.nextInt();         System.out.println(a/b);     } }

---
+++ Division.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class division
+{
+    public static void main(String[] args)
+    {
+        int a,b;
+        Scanner read=new Scanner(System.in);
+        a=read.nextInt();
+        b=read.nextInt();
+        System.out.println(a/b);
+    }
+}
